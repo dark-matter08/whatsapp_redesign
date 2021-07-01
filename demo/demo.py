15b52f94c531156b60b456145dd429152af17447
dark-matter08 <chelucien08@gmail.com>
2021-07-01 23:16:10 +0100

    Done with the Chat list screen. Starting user chat screen

--- demo/demo.py
+++ demo/demo.py
@@ -1,23 +1,23 @@
 chats = {
 
     "anita": ["hey;2:20 PM;read;you", "hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
-        "staying alive;2:20 PM;delivered;you"],
+        "staying alive;2:20 PM;new;friend"],
     "jooyce": ["what's the deal with that guy from earlier;2:20 PM;read;you", "seriously i dont know;2:20 PM;read;friend", "we re not good enough to help move? Lol.;2:20 PM;read;you", "Lol guess we re nt;2:20 PM;read;friend",
         "you coming to bariat's;2:20 PM;read;you","yeah what time?;2:50 PM;read;friend", "hope its not late ;2:58 PM;read;friend"],
     "weeknd": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
-        "he really made a fool out of me;2:20 PM;delivered;you"],
+        "he really made a fool out of me;2:20 PM;waiting;you"],
     "jordan": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
-        "you keep crying to me;2:20 PM;delivered;you"],
+        "you keep crying to me;2:20 PM;new;friend"],
     "breezy": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
         "staying alive;2:20 PM;delivered;you"],
     "wizkid": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
-        "hopefully something comes up;2:20 PM;delivered;you"],
+        "hopefully something comes up;2:20 PM;sent;you"],
     "davido": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
-        "i am down here;2:20 PM;delivered;you"],
+        "i am down here;2:20 PM;sent;you"],
     "zendaya": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
         "what tf were you thinking ;2:20 PM;delivered;you"],
     "rihanna": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend", "how are you doing?;2:20 PM;read;you","doing great you?;2:20 PM;waiting;friend",
-        "i won bro;2:20 PM;delivered;you"],
+        "i won bro;2:20 PM;read;friend"],
 }
 
 anita = {
@@ -29,49 +29,49 @@ anita = {
 
 jooyce = {
     "name": "Jooyce",
-    "image": "assets/images/16.jpg",
+    "image": "assets/images/16.jpeg",
     "active": False,
     "msg": chats["jooyce"],
     }
 
 weeknd = {
     "name": "Weeknd",
-    "image": "assets/images/weeknd.jpg",
+    "image": "assets/images/weeknd.jpeg",
     "active": True,
     "msg": chats["weeknd"],
     }
 
 jordan = {
     "name": "Jordan",
-    "image": "assets/images/jordan.jpg",
+    "image": "assets/images/jordan.jpeg",
     "active": False,
     "msg": chats["jordan"],
     }
 
 breezy = {
     "name": "Breezy",
-    "image": "assets/images/breezy.jpg",
+    "image": "assets/images/breezy.jpeg",
     "active": True,
     "msg": chats["breezy"],
     }
 
 wizkid = {
     "name": "Wizkid",
-    "image": "assets/images/wizkid.jpg",
+    "image": "assets/images/wizkid.jpeg",
     "active": False,
     "msg": chats["wizkid"],
     }
 
 davido = {
     "name": "Davido",
-    "image": "assets/images/davido.jpg",
+    "image": "assets/images/davido.jpeg",
     "active": False,
     "msg": chats["davido"],
     }
 
 zendaya = {
     "name": "Zendaya",
-    "image": "assets/images/14.jpg",
+    "image": "assets/images/14.jpeg",
     "active": False,
     "msg": chats["zendaya"],
     }
@@ -85,6 +85,6 @@ rihanna = {
 
 profiles = [anita, jooyce, weeknd, jordan, breezy, wizkid, davido, zendaya, rihanna]
 
-print(profiles[0]["msg"])
+# print(profiles[0]["msg"])
 # print(anita.items())
 # chats.items
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.properties import ObjectProperty, ListProperty, StringProperty
+from kivymd.uix.card import MDCard
+from kivy.properties import OptionProperty, DictProperty, StringProperty, BooleanProperty
 from kivy.lang.builder import Builder
-from demo import demo
+from demo.demo import profiles
 
 Builder.load_file('story.kv')
+Builder.load_file('chat_list.kv')
+Builder.load_file('chat_screen.kv')
 Window.size = (365, 600)
 
 
+# =========== Message Screen classes ==========
 class MessageScreen(Screen):
     """A Screen that display all stories and message history"""
 
@@ -23,19 +27,74 @@ class StoryWithIcon(MDBoxLayout):
     text = StringProperty()
     icon = StringProperty()
 
+class ChatListItem(MDCard):
+    """A clickable row for chats"""
+    message = StringProperty()
+    friend_avatar = StringProperty()
+    friend_name = StringProperty()
+    time_stamp = StringProperty()
+    status_icon = StringProperty()
+    profile = DictProperty()
+    is_read = OptionProperty(None, options=['delivered', 'read', 'new', 'waiting', 'sent'])
+
+# =========== Chat screen classes =============
+class ChatScreen(Screen):
+    """"A screen that displays chat with a user"""
+    text = StringProperty()
+    image = StringProperty()
+    active = BooleanProperty()
+
 class ChatApp(MDApp):
     def build(self):
         # load the kv file
         self.load_kv("main.kv")
+        self.story_builder()
+        self.chat_list_builder()
 
         # setting theme properties
+
         self.theme_cls.primary_palete = 'Teal' #Main color
-        self.theme_cls.theme_style = 'Dark' #Dark theme
-        self.theme_cls.accent_color  #Second color with 400 hue value
+        self.theme_cls.theme_style = 'Light' #Dark theme
+        # self.theme_cls.accent_color = 'Teal'  #Second color with 400 hue value
         self.theme_cls.accent_hue = '400'
         self.title = 'WhatsApp Redesign'
 
+    def story_builder(self):
+        # create story for each user and add to the layout
+        story_item = self.root.ids.message_screen.ids.story_layout
+        print(story_item)
+        for profile in profiles:
+            story_item.add_widget(
+                StoryWithImage(
+                    text = profile['name'],
+                    source = profile['image']
+                )
+            )
 
+    def chat_list_builder(self):
+        chat_time_line = self.root.ids.message_screen.ids.chat_time_line
+        for profile in profiles:
+            for message in profile['msg']:
+                last_message, time, is_read, sender = message.split(';')
+                icons = {
+                    'read': 'checkbox-multiple-marked-circle',
+                    'delivered': 'checkbox-multiple-marked-circle-outline',
+                    'new':'numeric-1-circle',
+                    'sent':'check-circle-outline',
+                    'waiting':'dots-horizontal-circle-outline'}
+                status_icon = icons[is_read] if is_read in icons.keys() else ''
+
+            chat_time_line.add_widget(
+                    ChatListItem(
+                        profile = profile,
+                        friend_name = profile['name'],
+                        friend_avatar = profile['image'],
+                        message = last_message,
+                        time_stamp = time,
+                        is_read = is_read,
+                        status_icon = status_icon
+                    )
+                )
 
 
 ChatApp().run()
